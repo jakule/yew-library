4441dcafe97a31ccd340b4771bf4485509e7d8f8
Jakub Nyckowski <nyckowski.jakub@gmail.com>
2021-08-25 20:51:49 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+    SubtractOne,
+}
+
+struct Model {
+    // `ComponentLink` is like a reference to a component.
+    // It can be used to send messages to the component
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+            Msg::SubtractOne => {
+                self.value -= 1;
+
+                true
+            }
+        }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+                <h1>{ "Hello World!" }</h1>
+                <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <button onclick=self.link.callback(|_| Msg::SubtractOne)>{ "-1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
